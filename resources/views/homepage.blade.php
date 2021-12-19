f749eacb42db2c25a5e78f6985e069ddcc85b556
rota99C <rotaboolean@gmail.com>
2021-12-19 22:09:10 +0100

    main working progress

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,4 +4,18 @@
 
 @section('content')
 
+<div class="w-75 m-auto d-flex flex-wrap ">
+    <div class="current-serie">
+      <p><strong>CURRENT SERIES</strong></p>
+    </div>
+    @foreach($comics as $comic)
+        <div class="singolo">
+            <img src="{{$comic['thumb']}}" alt="">
+          <h5>{{$comic ['series']}}</h5>  
+        </div>
+        
+        @endforeach
+
+</div>
+
 @endsection
